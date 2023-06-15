c2649b290692b87a06a9e333625b4c67e069b281
Łukasz Matczuk <lukasz.matczuk84@gmail.com>
2023-06-15 15:22:46 +0200

    Stworzenie projektu

---
+++ src/main/java/com/example/zadanie18/Zadanie18Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie18Application.class, args);
+    }
+
+}
